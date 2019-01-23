289c8e3afd072aa34f41a3120103865d13d3a7b0
Kevin Chin <chinkevin25@gmail.com>
2019-01-22 22:07:29 -0800

    update aw link

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 app.use(bodyParser());
 
-app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/api/reservations/', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.153.120.187:3002', changeOrigin: true }));
+app.use('/api/reservations/', proxy({ target: 'http://54.153.120.187:3002', changeOrigin: true }));
 // app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://54.183.92.72:3004', changeOrigin: true }));
 // app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
 // app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
